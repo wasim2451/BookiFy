d5331ebd3228c86e66e11f0db247c9c73e304e1d
[Wasim Akhtar Khan] <wasimakhtarkhan2003@gmail.com>
2025-07-08 01:17:35 +0530

    Navbar Sign Up problem fixed !

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,21 +21,26 @@ const CustomNavbar = () => {
 
         <Navbar.Collapse id="basic-navbar-nav">
           
-          <Nav className="me-auto flex-column flex-lg-row align-items-center align-items-lg-center  gap-lg-4">
+          <Nav className="me-auto flex-column flex-lg-row align-items-center align-items-lg-center  gap-lg-3">
             <Nav.Link href="/" className="text-white" >
               Home
             </Nav.Link>
-            <Nav.Link href="/book-listing" className="text-white nav-link">
+            {isLoggedIn ? <Nav.Link href="/book-listing" className="text-white nav-link">
               List Your Book
-            </Nav.Link>
+            </Nav.Link>:<></>} 
           </Nav>
 
       
-          <Nav className="ms-auto d-flex flex-column flex-lg-row align-items-center align-items-lg-center gap-2 gap-lg-3 mt-3 mt-lg-0">
+          <Nav className="ms-auto d-flex flex-column flex-lg-row align-items-center align-items-lg-center gap-2 gap-lg-3 mt-lg-0">
             {!isLoggedIn ? (
-              <Nav.Link href="/signin" className="text-white">
-                <Button variant="outline-light">Sign In</Button>
-              </Nav.Link>
+                <div className="d-flex flex-lg-row flex-column">
+                    <Nav.Link href="/signin" className="text-white">
+                     <Button variant="outline-light" style={{width:"100px"}}>Sign In</Button>
+                    </Nav.Link>
+                    <Nav.Link href="/register" className="text-white">
+                     <Button variant="outline-light" style={{width:"100px"}}>Sign up</Button>
+                    </Nav.Link>
+                </div>
             ) : (
               <>
                 {src && (
@@ -55,6 +60,7 @@ const CustomNavbar = () => {
                 <Button
                   variant="outline-light"
                   size="sm"
+                  style={{width:"100px"}}
                   className="mb-3 mb-lg-0"
                   onClick={async() => {
                     try{
