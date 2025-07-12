acfc135f4125d5d931148f8390969179ac6817c9
[Wasim Akhtar Khan] <wasimakhtarkhan2003@gmail.com>
2025-07-13 02:48:07 +0530

    Final commit 2

--- src/context/payment.js
+++ src/context/payment.js
@@ -3,13 +3,13 @@ import axios from "axios";
 export const paymentFunction = async (object) => {
     // All Payment code to be written here !
     console.log(object);
-    const res = await axios.post('http://localhost:3000/api/createorder', object);
+    const res = await axios.post('https://lambent-florentine-b5d8b8.netlify.app/api/createorder', object);
     console.log(res);
     return res.data.orderID;
 }
 
 export const emailFunction = async (orderData) => {
-    const status = await axios.post('http://localhost:3000/api/v1/send/email',orderData);
+    const status = await axios.post('https://lambent-florentine-b5d8b8.netlify.app/api/v1/send/email',orderData);
     console.log(status.data.check,"comes from Backend !"); // true or false
     return status.data.check;  
 }
