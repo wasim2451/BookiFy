70dbdf17e9b5de45f70569366a6d625863128017
Wasim Akhtar Khan <122202006+wasim2451@users.noreply.github.com>
2025-08-16 01:34:54 +0530

    Update payment.js

--- src/context/payment.js
+++ src/context/payment.js
@@ -3,13 +3,13 @@ import axios from "axios";
 export const paymentFunction = async (object) => {
     // All Payment code to be written here !
     console.log(object);
-    const res = await axios.post('https://bookify-1z0e.onrender.com/api/createorder', object);
+    const res = await axios.post('https://bookify-backend-production-22f0.up.railway.app/api/createorder', object);
     console.log(res);
     return res.data.orderID;
 }
 
 export const emailFunction = async (orderData) => {
-    const status = await axios.post('https://bookify-1z0e.onrender.com/api/v1/send/email',orderData);
+    const status = await axios.post('https://bookify-backend-production-22f0.up.railway.app/api/v1/send/email',orderData);
     console.log(status.data.check,"comes from Backend !"); // true or false
     return status.data.check;  
 }
